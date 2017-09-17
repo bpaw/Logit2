14ed5fbfdafe9435205f780b87494e76208c3023
Brandon <paw.brandon@yahoo.com>
2017-09-17 12:16:30 -0700

    set up routing and controllers for root

--- config/express.js
+++ config/express.js
@@ -2,5 +2,8 @@ var express = require('express');
 module.exports = function() {
     var app = express();
     require('../app/routes/index.server.routes.js')(app);
+    app.set('views', './app/views');
+	app.set('view engine', 'ejs');
+	app.use(express.static('./public'));
     return app;
 };
